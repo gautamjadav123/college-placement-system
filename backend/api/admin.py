883488a8e78ad0c15434d276d4040a95475e7d2f
Gautam Jadhav <80582714+gautamjadav123@users.noreply.github.com>
2026-05-19 23:44:38 +0530

    Update admin.py

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,28 +1,39 @@
 from django.contrib import admin
 from .models import Profile, Job, Application, Interview, Notification, PlacementStat
 
+# Admin panel customization
+admin.site.site_header = "S. R. Government Polytechnic College, Sagar"
+admin.site.site_title = "SRGP Admin Portal"
+admin.site.index_title = "Welcome to Placement Management System"
+
+
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['user', 'role', 'branch', 'cgpa']
     list_filter = ['role', 'branch']
+
 
 @admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
     list_display = ['title', 'company', 'ctc', 'approved', 'created_at']
     list_filter = ['approved']
 
+
 @admin.register(Application)
 class ApplicationAdmin(admin.ModelAdmin):
     list_display = ['student', 'job', 'status', 'applied_at']
     list_filter = ['status']
 
+
 @admin.register(Interview)
 class InterviewAdmin(admin.ModelAdmin):
     list_display = ['application', 'date', 'venue']
 
+
 @admin.register(Notification)
 class NotificationAdmin(admin.ModelAdmin):
     list_display = ['user', 'message', 'is_read', 'created_at']
+
 
 @admin.register(PlacementStat)
 class PlacementStatAdmin(admin.ModelAdmin):
